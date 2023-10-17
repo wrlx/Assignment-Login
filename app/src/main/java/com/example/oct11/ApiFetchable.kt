eef49819bbcc43e5232b1848b495ccb3e83f0e8c
wrlx <jinutk25846@gmail.com>
2023-10-17 09:24:26 +0530

    7th day commit(oct16)

---
+++ app/src/main/java/com/example/oct11/ApiFetchable.kt
@@ -0,0 +1,12 @@
+package com.example.oct11
+
+
+import okhttp3.Call
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface ApiFetchable {
+    @GET("/v2/quotes")
+    suspend fun getData() : Response<List<String>>
+
+}
