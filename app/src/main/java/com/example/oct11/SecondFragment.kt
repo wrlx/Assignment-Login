f19f8674cf6d61f4f2a192755f724dd78797f346
wrlx <jinutk25846@gmail.com>
2023-10-18 12:58:14 +0530

    8th day commit no. 1 (oct18)

--- app/src/main/java/com/example/oct11/SecondFragment.kt
+++ app/src/main/java/com/example/oct11/SecondFragment.kt
@@ -1,11 +1,23 @@
 package com.example.oct11
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ProgressBar
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.oct11.R
+import com.example.oct11.recyclerOnline.AdapterOnline
+import com.example.oct11.recyclerOnline.CarApiFetchable
+import com.example.oct11.recyclerOnline.RetrofitHelperCar
+import com.example.oct11.recyclerOnline.dataclass.ItemsViewModelOnline
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +30,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SecondFragment : Fragment() {
+
+    val dataOnline = ArrayList<ItemsViewModelOnline>()
+    lateinit var adapter: AdapterOnline
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,10 +50,45 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        val rootView = inflater.inflate(R.layout.activity_main_recycler_online, container, false)
+        return rootView
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val carApi = RetrofitHelperCar.getInstance().create(CarApiFetchable::class.java)
 
+        val recyclerview = view.findViewById<RecyclerView>(R.id.recyclerviewonline)
+        val progerssBar = view.findViewById<ProgressBar>(R.id.progressBarRecycler)
+
+        recyclerview.layoutManager = LinearLayoutManager(requireContext())
+
+        GlobalScope.launch {
+//            progerssBar.visibility = View.VISIBLE
+            val result = carApi.getCarData()
+
+            progerssBar.visibility = View.INVISIBLE
+
+            if (result != null) {
+                val temp = result.body()?.Results
+
+                if (temp != null) {
+                    for (item in temp) {
+                        val country = item.Country
+                        val mfrName = item.Mfr_Name
+                        dataOnline.add(ItemsViewModelOnline(mfrName, country))
+                    }
+                    requireActivity().runOnUiThread {
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+            }
+        }
+
+        adapter = AdapterOnline(dataOnline)
+        recyclerview.adapter = adapter
+//        progerssBar.visibility = View.GONE
     }
 
     companion object {
